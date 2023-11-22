15d046bd16fb8026fb90d52efe0358b09da37af3
44451516 <103430341+44451516@users.noreply.github.com>
2023-11-22 13:17:54 +0800

    1
    1

---
+++ RaidBuff.cs
@@ -0,0 +1,19 @@
+namespace XIVSlothCombo.Combos.PvE;
+
+public class RaidBuff
+{
+    internal const ushort
+
+        // Heals
+        连环计 = 1221,
+        
+        
+        
+        //忍者夺取
+        攻其不备 = 3254,
+        受伤加重 = 638,
+        
+        
+        
+        留空 = 10024;
+}
